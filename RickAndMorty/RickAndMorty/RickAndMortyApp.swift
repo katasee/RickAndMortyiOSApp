54dc3bcb6f4807d21f00987aca822fff8c94a116
katasee <kotalov.developer@gmail.com>
2025-05-30 17:45:23 +0200

    Init Xcode project

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Anton on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
